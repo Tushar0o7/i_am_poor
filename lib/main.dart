1138610d0098a46e2ce7f1d3994db37c4e581de0
TUSHAR BASAK <72143088+Tushar0o7@users.noreply.github.com>
2022-05-13 17:11:51 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        appBar: AppBar(
+          title: const Text('I Am Poor'),
+          backgroundColor: Colors.amber,
+        ),
+        body: const Center(
+          child: Image(image: AssetImage('images/poo.png')),
+        ),
+      ),
+    );
+  }
+}
